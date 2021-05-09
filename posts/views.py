40d6468597a61eb8893951526587b89bddb3bf34
Dmitriy <dbryndin88@yandex.ru>
2021-05-09 12:25:51 +0300

    2

--- posts/views.py
+++ posts/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import status
-from .models import Post, Comment
+from .models import Post
 from .serializers import PostSerializer, CommentSerializer
 
 
